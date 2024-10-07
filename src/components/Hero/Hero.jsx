30fb3315acc7f5066fd5308107346e7fde535978
khaled <khaledelgoker382@gmail.com>
2024-10-07 16:09:49 +0300

    edit p

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -12,6 +12,8 @@ const Hero = () => {
         <div id='home' className='hero'>
             <img src={profile_img} alt="" />
             <h1><span>I’m Khaled Nasser, frontend developer based in Egypt.</span></h1>
+            <p>Highly skilled front-end developer with expertise in creating responsive, user-friendly web applications using React, JavaScript, and CSS frameworks. Experienced in working with cross-functional teams to deliver high-quality web solutions.
+            </p>
             <div className="hero-action">
                 <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
                 {/* <div className="hero-resume">My resume</div> */}
